cb8a5321de44dbc03f26940bdfdfe34d6021de65
nahidhasan330874 <nahidhasan1005004@gmail.com>
2026-02-22 13:16:07 +0600

    created the toggle btn

--- script/scirpt.js
+++ script/scirpt.js
@@ -10,20 +10,30 @@ function allCards() {
   jod.innerText = allCardsSection.children.length;
 }
 allCards();
+
+
+// btn toggle Style
+
+const allBtn = document.getElementById('btn-all');
+const interviewBtn = document.getElementById('btn-interview');
+const rejectedBtn = document.getElementById('btn-rejected');
+
+function toggle(id) {
+
+  allBtn.classList.remove("bg-[#3B82F6]", "text-white");
+  interviewBtn.classList.remove("bg-[#3B82F6]", "text-white");
+  rejectedBtn.classList.remove("bg-[#3B82F6]", "text-white");
+
+  allBtn.classList.add("btn", "text-[#64748B]");
+  interviewBtn.classList.add("btn", "text-[#64748B]");
+  rejectedBtn.classList.add("btn", "text-[#64748B]");
+
+  
+  const selected = document.getElementById(id);
+
+  selected.classList.add("bg-[#3B82F6]", "text-white");
+}
+
  
-  const buttons = document.querySelectorAll(".btn-file");
 
-  buttons.forEach(button => {
-    button.addEventListener("click", () => {
-
-      buttons.forEach(btn => {
-        btn.classList.remove("bg-[#3B82F6]", "text-white");
-        btn.classList.add("btn-outline", "text-[#64748B]");
-      })
-      button.classList.remove("btn-outline",'text-[#64748B]');
-      button.classList.add("bg-[#3B82F6]", "text-white");
-    });
-  });
-  
-
-  
+ 
