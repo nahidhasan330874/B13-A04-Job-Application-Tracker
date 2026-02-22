2994de0fb7511b20fd8bcc098f7f21db0b07f822
nahidhasan330874 <nahidhasan1005004@gmail.com>
2026-02-22 13:15:39 +0600

    created the toggle btn

--- script/scirpt.js
+++ script/scirpt.js
@@ -1,10 +1,29 @@
-let total = document.getElementById('total');
-let  Interview = document.getElementById('Interview');
-let  Rejected = document.getElementById(' Rejected');
+let total = document.getElementById("total");
+let Interview = document.getElementById("Interview");
+let Rejected = document.getElementById(" Rejected");
+let jod = document.getElementById("jobs");
 
-const allCardsSection = document.getElementById('all-cards');
+const allCardsSection = document.getElementById("all-cards");
 
-function allCards(){
-    total.innerText = allCardsSection.children.length ;
+function allCards() {
+  total.innerText = allCardsSection.children.length;
+  jod.innerText = allCardsSection.children.length;
 }
 allCards();
+ 
+  const buttons = document.querySelectorAll(".btn-file");
+
+  buttons.forEach(button => {
+    button.addEventListener("click", () => {
+
+      buttons.forEach(btn => {
+        btn.classList.remove("bg-[#3B82F6]", "text-white");
+        btn.classList.add("btn-outline", "text-[#64748B]");
+      })
+      button.classList.remove("btn-outline",'text-[#64748B]');
+      button.classList.add("bg-[#3B82F6]", "text-white");
+    });
+  });
+  
+
+  
