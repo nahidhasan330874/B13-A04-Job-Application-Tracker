ecd9bf4b30798a0092464846fbe103180d5403f4
nahidhasan330874 <nahidhasan1005004@gmail.com>
2026-02-23 12:28:16 +0600

    added the rejected section and same edits

--- script/machine.js
+++ script/machine.js
@@ -4,6 +4,15 @@ const allBtn = document.getElementById('btn-all');
 const interviewBtn = document.getElementById('btn-interview');
 const rejectedBtn = document.getElementById('btn-rejected');
 
+document.addEventListener("click", function (deleted) {
+  if (deleted.target.closest(".btn-delete")) {
+    const card = deleted.target.closest("#Interview-btn");
+    card.remove();
+
+     allCards();
+  }
+});
+
 function toggle(id) {
 
   allBtn.classList.remove("bg-[#3B82F6]", "text-white");
@@ -13,53 +22,60 @@ function toggle(id) {
   allBtn.classList.add("btn", "text-[#64748B]");
   interviewBtn.classList.add("btn", "text-[#64748B]");
   rejectedBtn.classList.add("btn", "text-[#64748B]");
-
-    const interview = document.getElementById('Interview-file');
+  
   
   const selected = document.getElementById(id);
-
+  currentStatus =id ;
   selected.classList.add("bg-[#3B82F6]", "text-white");
 
     if(id == 'btn-interview'){
-      interview.classList.remove('hidden');
       allCardsSection.classList.add('hidden');
       filteredSection.classList.remove('hidden');
+
+       interviewTracker();
     }
     else if ( id == "btn-all"){
         allCardsSection.classList.remove('hidden');
         filteredSection.classList.add('hidden')
+    }else if( id == "btn-rejected"){
+      allCardsSection.classList.add('hidden');
+      filteredSection.classList.remove('hidden');
+      if(currentStatus === 'btn-interview') interviewTracker();
+       if(currentStatus === 'btn-rejected') rejectedTracker();
+      
     }
-
+    
+ allCards();
 }
 
 
 // hide all / show id
 function showOnly(id){
     const allCards = document.getElementById('all-cards');
-    const interview = document.getElementById('Interview-file');
-    const rejected = document.getElementById('Rejected-file');
+    const interviews = document.getElementById('Interview-file');
+    const reject = document.getElementById('Rejected-file');
+
 
     allCards.classList.add('hidden');
-    interview.classList.add('hidden');
-    rejected.classList.add('hidden');
+    interviews.classList.add('hidden');
+    reject.classList.add('hidden');
 
     const selectedBtn = document.getElementById(id);
     selectedBtn.classList.remove('hidden');
 
      if(id == 'card-btn-interview'){
        allCardsSection.classList.remove('hidden');
-       interview.classList.add('hidden');
       filteredSection.classList.add('hidden');
     }  
-    else if(id == 'btn-interview'){
-      allCardsSection.classList.add('hidden');
-      interview.classList.add('hidden');
-      filteredSection.classList.remove('hidden');
-    } else if ( id == "btn-all"){
-        interview.classList.add('hidden');
+    else if(id == 'btn-all'){
+      allCardsSection.classList.remove('hidden');
+      filteredSection.classList.add('hidden');
+    } else if( id == "card-btn-rejected"){
         allCardsSection.classList.remove('hidden');
-        filteredSection.classList.add('hidden')
-    }
+        filteredSection.classList.add('hidden');
+    };
+
 }
+
 
  
--- script/scirpt.js
+++ script/scirpt.js
@@ -1,5 +1,6 @@
 let interview = [];
 let rejected = [];
+let currentStatus = 'all';
 
 let total = document.getElementById("total");
 let Interview = document.getElementById("Interview");
@@ -15,22 +16,22 @@ function allCards() {
   total.innerText = allCardsSection.children.length;
   jod.innerText = allCardsSection.children.length;
   Interview.innerText = interview.length;
-  rejected.innerText = rejected .length;
- 
+  //  jod.innerText = interview.length;
+  Rejected.innerText = rejected.length;
+  //  jod.innerText = rejected.length;
+
 }
 function alCard(){
-  jod.innerText = allCardsSection.children.length;
+   jod.innerText = allCardsSection.children.length;
 }
 function interCard(){
    jod.innerText = interview.length;
 }
-
 function rejCard(){
    jod.innerText = rejected.length;
 }
-allCards();
+ allCards()
  
-
 mainContainer.addEventListener("click", function (event) {
   if (event.target.classList.contains("card1-interview")) {
     const parentNode = event.target.parentNode.parentNode;
@@ -39,31 +40,66 @@ mainContainer.addEventListener("click", function (event) {
     const Explain = parentNode.querySelector(".card2-Explain").innerText;
     const money = parentNode.querySelector(".card1-money").innerText;
     const button = parentNode.querySelector(" .card1-btn").innerText;
-    const texts = parentNode.querySelector(" .card1-lisen").innerText;
+    const texts = parentNode.querySelector(".card1-lisen")?.innerText;
     
-    parentNode.querySelector('.card1-btn').innerHTML = ` <button id="btn-not-applied" class=" btn btn-sm  btn-active border-none btn-success  ">
+    parentNode.querySelector('.card1-btn').innerHTML = ` <button id="btn-not-applied" class=" btn btn-sm  btn-active border-none cursor-default btn-success  ">
               INTERVIEWED
             </button>`
     const cardInfo = {
       head,
       Explain,
       money,
-      button:` <button id="btn-not-applied" class=" btn btn-sm  btn-active border-none btn-success  ">
+      button :` <button id="btn-not-applied" class=" btn btn-sm  btn-active border-none cursor-default btn-success  ">
               INTERVIEWED
             </button>`,
       texts
       
     };
-    const exist = interview.find((item) => item.head == cardInfo.head);
+    const exist = interview.find(item => item.head == cardInfo.head);
     
     
     if (!exist) {
       interview.push(cardInfo);
     }
+    
+    rejected = rejected.filter(item => item.head != cardInfo.head);
+     if(currentStatus === 'btn-rejected') rejectedTracker();
 
--  allCards();
-   
+     allCards();
     interviewTracker();
+  } else if (event.target.classList.contains("card2-rejected")) {
+    const parentNode = event.target.parentNode.parentNode;
+
+    const head = parentNode.querySelector(" .card1-head").innerText;
+    const Explain = parentNode.querySelector(".card2-Explain").innerText;
+    const money = parentNode.querySelector(".card1-money").innerText;
+    const button = parentNode.querySelector(".card1-btn").innerText;
+    const texts = parentNode.querySelector(".card1-lisen")?.innerText;
+    
+    parentNode.querySelector('.card1-btn').innerHTML = ` <button class=" card2-rejected btn btn-active border-none px-3 cursor-default btn-error btn-sm">
+    REJECTED</button>`;
+    const cardInfo = {
+      head,
+      Explain,
+      money,
+      button: ` <button class=" card2-rejected btn btn-active border-none px-3 cursor-default  btn-error btn-sm">
+            REJECTED</button>`,
+      texts
+      
+    };
+    const exist = rejected.find(item => item.head == cardInfo.head);
+    
+    
+    if (!exist) {
+      rejected.push(cardInfo);
+    }
+    interview = interview.filter(item=> item.head != cardInfo.head);
+
+    if(currentStatus === 'btn-interview') interviewTracker();
+    
+
+    allCards();
+   
   }
 });
 
@@ -100,15 +136,17 @@ function interviewTracker() {
               ${inter.button}
             </button>
             </div>
-            <p class=" text-[#323B49] text-[14px] pt-1">
+            <div class="card1-lisen">
+             <p class=" text-[#323B49] text-[14px] pt-1">
               ${inter.texts}
-            </p>
+              </p>
+             </div>
 
-          <div class="pt-4">
-            <button class="card1-interview btn btn-outline btn-success btn-sm">
+           <div class="pt-4">
+            <button onclick ="interCard()"  id="card-btn-interview" class="card1-interview btn btn-outline btn-success btn-sm">
               INTERVIEW
             </button>
-            <button class=" card1-rejected btn btn-outline btn-error btn-sm">REJECTED</button>
+            <button  id="card-btn-rejected" class=" card2-rejected btn btn-outline btn-error btn-sm">REJECTED</button>
           </div>
 
     
@@ -116,3 +154,59 @@ function interviewTracker() {
     filteredSection.appendChild(div)
   }
 }
+function rejectedTracker() {
+  filteredSection.innerHTML = "";
+
+  for (let reject of rejected) {
+    let div = document.createElement("div");
+    div.className = "bg-base-100 p-6 mt-4 rounded-xl shadow";
+    div.innerHTML = `
+       <div class="flex justify-between">
+            <div>
+              <h2 class="card1-head text-[#002C5C] text-[18px] font-semibold">
+                ${reject.head}
+              </h2>
+              <p class="card2-Explain text-[#64748B]">${reject.Explain}</p>
+            </div>
+
+            <div class="flex items-center">
+              <button
+                class="btn-delete btn rounded-full px-3 btn-base-100 text-[#64748B] shadow"
+              >
+                <i class="fa-regular fa-trash-can"></i>
+              </button>
+            </div>
+          </div>
+
+          <div class=" card1-money text-[#64748B] text-[14px] py-4">
+            <p> ${reject.money}</p>
+          </div>
+
+          <div class="card1-btn">
+            <button id="btn-not-applied" class=" btn btn-sm btn-active text-[#002C5C] bg-[#e5eaf1] border-none">
+              ${reject.button}
+            </button>
+            </div>
+           <div class="card1-lisen">
+             <p class=" text-[#323B49] text-[14px] pt-1">
+              ${reject.texts}
+              </p>
+           </div>
+
+
+          <div class="pt-4">
+            <button  id="card-btn-interview" class="card1-interview btn btn-outline btn-success btn-sm">
+              INTERVIEW
+            </button>
+            <button  id="card-btn-rejected" class=" card2-rejected btn btn-outline btn-error btn-sm">REJECTED</button>
+          </div>
+
+    
+    `;
+    filteredSection.appendChild(div)
+
+  }
+    allCards();
+}
+
+
